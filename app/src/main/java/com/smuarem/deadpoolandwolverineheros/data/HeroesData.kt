5e5afd25bcee477c19fa82eab30c2c6ed8b7ade9
maharram sadikhli <s.muarem10@gmail.com>
2024-07-30 19:51:08 +0400

    created hero data

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/data/HeroesData.kt
@@ -0,0 +1,110 @@
+package com.smuarem.deadpoolandwolverineheros.data
+
+import com.smuarem.deadpoolandwolverineheros.R
+import com.smuarem.deadpoolandwolverineheros.model.Hero
+
+object HeroesData {
+    val deadpool = Hero(
+        heroName = R.string.deadpool_name,
+        heroActor = R.string.deadpool_actor,
+        heroSuperpower = R.string.deadpool_superpower,
+        heroImage = R.drawable.deadpool1
+    )
+    val wolverine = Hero(
+        heroName = R.string.wolverine_name,
+        heroActor = R.string.wolverine_actor,
+        heroSuperpower = R.string.wolverine_superpower,
+        heroImage = R.drawable.wolverine
+    )
+    val cassandraNova = Hero(
+        heroName = R.string.cassandra_nova_name,
+        heroActor = R.string.cassandra_nova_actor,
+        heroSuperpower = R.string.cassandra_nova_superpower,
+        heroImage = R.drawable.cassandranova
+    )
+    val mrParadox = Hero(
+        heroName = R.string.mr_paradox_name,
+        heroActor = R.string.mr_paradox_actor,
+        heroSuperpower = R.string.mr_paradox_superpower,
+        heroImage = R.drawable.paradox
+    )
+    val storm = Hero(
+        heroName = R.string.storm_name,
+        heroActor = R.string.storm_actor,
+        heroSuperpower = R.string.storm_superpower,
+        heroImage = R.drawable.storm
+    )
+    val x23 = Hero(
+        heroName = R.string.x_23_name,
+        heroActor = R.string.x_23_actor,
+        heroSuperpower = R.string.x_23_superpower,
+        heroImage = R.drawable.x_23
+    )
+    val ladyDeadpool = Hero(
+        heroName = R.string.lady_deadpool_name,
+        heroActor = R.string.lady_deadpool_actor,
+        heroSuperpower = R.string.lady_deadpool_superpower,
+        heroImage = R.drawable.ladydeadpool
+    )
+    val elektra = Hero(
+        heroName = R.string.elektra_name,
+        heroActor = R.string.elektra_actor,
+        heroSuperpower = R.string.elektra_superpower,
+        heroImage = R.drawable.elektra
+    )
+    val negasonic = Hero(
+        heroName = R.string.negasonic_teenage_warhead_name,
+        heroActor = R.string.negasonic_teenage_warhead_actor,
+        heroSuperpower = R.string.negasonic_teenage_warhead_superpower,
+        heroImage = R.drawable.negasonic
+    )
+    val yukio = Hero(
+        heroName = R.string.yukio_name,
+        heroActor = R.string.yukio_actor,
+        heroSuperpower = R.string.yukio_superpower,
+        heroImage = R.drawable.yukio
+    )
+    val colossus = Hero(
+        heroName = R.string.colossus_name,
+        heroActor = R.string.colossus_actor,
+        heroSuperpower = R.string.colossus_superpower,
+        heroImage = R.drawable.colossus
+    )
+    val pyro = Hero(
+        heroName = R.string.pyro_name,
+        heroActor = R.string.pyro_actor,
+        heroSuperpower = R.string.pyro_superpower,
+        heroImage = R.drawable.pyro
+        )
+    val dogpool = Hero(
+        heroName = R.string.dogpool_name,
+        heroActor = R.string.dogpool_actor,
+        heroSuperpower = R.string.dogpool_superpower,
+        heroImage = R.drawable.dogpool
+    )
+    val hunterB15 = Hero(
+        heroName = R.string.hunter_b_15_name,
+        heroActor = R.string.hunter_b_15_actor,
+        heroSuperpower = R.string.hunter_b_15_superpower,
+        heroImage = R.drawable.b_15
+    )
+    val azazel = Hero(
+        heroName = R.string.azazel_name,
+        heroActor = R.string.azazel_actor,
+        heroSuperpower = R.string.azazel_superpower,
+        heroImage = R.drawable.azazel
+    )
+    val ladyDeathstrike = Hero(
+        heroName = R.string.lady_deathstrike_name,
+        heroActor = R.string.lady_deathstrike_actor,
+        heroSuperpower = R.string.lady_deathstrike_superpower,
+        heroImage = R.drawable.ladydeathstrike
+    )
+    val sabretooth = Hero(
+        heroName = R.string.sabretooth_name,
+        heroActor = R.string.sabretooth_actor,
+        heroSuperpower = R.string.sabretooth_superpower,
+        heroImage = R.drawable.sabretooth
+    )
+
+}
