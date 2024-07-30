573055ed176db4e56a5ace22c978aa1f25fdc5c1
maharram sadikhli <s.muarem10@gmail.com>
2024-07-30 19:51:15 +0400

    created hero list

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/data/HeroesRepo.kt
@@ -0,0 +1,23 @@
+package com.smuarem.deadpoolandwolverineheros.data
+
+object HeroesRepo {
+    val heroes = listOf(
+        HeroesData.deadpool,
+        HeroesData.wolverine,
+        HeroesData.cassandraNova,
+        HeroesData.mrParadox,
+        HeroesData.storm,
+        HeroesData.x23,
+        HeroesData.ladyDeadpool,
+        HeroesData.elektra,
+        HeroesData.negasonic,
+        HeroesData.yukio,
+        HeroesData.colossus,
+        HeroesData.pyro,
+        HeroesData.dogpool,
+        HeroesData.hunterB15,
+        HeroesData.azazel,
+        HeroesData.ladyDeathstrike,
+        HeroesData.sabretooth
+    )
+}
