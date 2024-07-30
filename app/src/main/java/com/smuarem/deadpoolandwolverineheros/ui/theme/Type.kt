ecaf0f672170558c1dbee521b4e641306fec33d7
maharram sadikhli <s.muarem10@gmail.com>
2024-07-30 16:39:55 +0400

    added typography

--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/theme/Type.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/theme/Type.kt
@@ -2,18 +2,56 @@ package com.smuarem.deadpoolandwolverineheros.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.smuarem.deadpoolandwolverineheros.R
+
+val MarvelFont = FontFamily(Font(R.font.marvel_regular))
+val MarvelItalicFont = FontFamily(Font(R.font.marvel_italic, FontWeight.Normal, FontStyle.Italic))
+val MarvelBoldFont = FontFamily(Font(R.font.marvel_bold, FontWeight.Bold))
+val MarvelBoldItalicFont = FontFamily(Font(R.font.marvel_bold_italic, FontWeight.Bold, FontStyle.Italic))
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    /**
+     * Font For Hero Description
+     */
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MarvelFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    /**
+        * Font For Top App Bar
+    */
+    displayLarge = TextStyle(
+        fontFamily = MarvelBoldFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    ),
+    /**
+     * Font For Hero Name
+     */
+    displayMedium = TextStyle(
+        fontFamily = MarvelBoldItalicFont,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        fontSize = 20.sp,
+    ),
+    /**
+     * Font For Actor Name
+     */
+    displaySmall = TextStyle(
+        fontFamily = MarvelItalicFont,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
+        fontSize = 18.sp,
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
