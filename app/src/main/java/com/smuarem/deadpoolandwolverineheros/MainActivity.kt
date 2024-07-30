501e7815c718fcd55bea96503c7d95733779b40d
maharram sadikhli <s.muarem10@gmail.com>
2024-07-30 19:12:45 +0400

    added heroes images

--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.smuarem.deadpoolandwolverineheros.ui.theme.AppTheme
 
@@ -36,6 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    Image(painter = painterResource(id = R.drawable.deadpool1), contentDescription = null)
 }
 
 @Preview(showBackground = true)
