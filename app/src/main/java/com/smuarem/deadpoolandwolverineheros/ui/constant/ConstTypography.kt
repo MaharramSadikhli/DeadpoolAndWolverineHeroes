ebef6bfdb116e67f34e3c7094d50a4723ff67dad
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 10:49:03 +0400

    constants for typography

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/constant/ConstTypography.kt
@@ -0,0 +1,30 @@
+package com.smuarem.deadpoolandwolverineheros.ui.constant
+
+import com.smuarem.deadpoolandwolverineheros.ui.theme.Typography
+
+
+/**
+ * Object containing typography styles for various UI elements.
+ */
+object ConstTypography {
+
+    /**
+     * Font for the top app bar.
+     */
+    val TOP_APP_BAR = Typography.displayLarge
+
+    /**
+     * Font for the hero's name.
+     */
+    val HERO_NAME = Typography.displayMedium
+
+    /**
+     * Font for the hero actor's name.
+     */
+    val HERO_ACTOR = Typography.displaySmall
+
+    /**
+     * Font for the hero description.
+     */
+    val DESCRIPTION = Typography.bodyLarge
+}
