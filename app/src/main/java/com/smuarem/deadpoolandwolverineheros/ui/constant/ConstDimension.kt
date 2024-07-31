d874716aa8b280a9dcc9a4b5655ca1b10351d10a
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 11:08:51 +0400

    created dimension consts

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/constant/ConstDimension.kt
@@ -0,0 +1,12 @@
+package com.smuarem.deadpoolandwolverineheros.ui.constant
+
+import com.smuarem.deadpoolandwolverineheros.R
+
+object ConstDimension {
+    val IMAGE_SIZE = R.dimen.image_size
+    val PADDING_SMALL = R.dimen.padding_small
+    val PADDING_MEDIUM = R.dimen.padding_medium
+    val RADIUS_SMALL = R.dimen.radius_small
+    val RADIUS_MEDIUM = R.dimen.radius_medium
+    val ELEVATION = R.dimen.elevation
+}
