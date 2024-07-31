c418ee479be8c7865014b3950e65e94eab1e3022
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 13:59:41 +0400

    added large dimens

--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/constant/ConstDimension.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/constant/ConstDimension.kt
@@ -6,6 +6,7 @@ object ConstDimension {
     val IMAGE_SIZE = R.dimen.image_size
     val PADDING_SMALL = R.dimen.padding_small
     val PADDING_MEDIUM = R.dimen.padding_medium
+    val PADDING_LARGE = R.dimen.padding_large
     val RADIUS_SMALL = R.dimen.radius_small
     val RADIUS_MEDIUM = R.dimen.radius_medium
     val ELEVATION = R.dimen.elevation
