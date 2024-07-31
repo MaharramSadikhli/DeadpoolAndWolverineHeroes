f21e765cef683ca6b5a9365173a3b894d88a5f42
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 11:08:20 +0400

    created shapes

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.smuarem.deadpoolandwolverineheros.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import com.smuarem.deadpoolandwolverineheros.ui.constant.ConstDimension
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(ConstDimension.RADIUS_SMALL),
+    medium = RoundedCornerShape(ConstDimension.RADIUS_MEDIUM),
+    large = RoundedCornerShape(ConstDimension.RADIUS_MEDIUM)
+)
