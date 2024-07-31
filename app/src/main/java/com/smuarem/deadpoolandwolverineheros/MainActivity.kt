26f09328e5735d8c478964754da7efebeeafdc6c
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 14:01:38 +0400

    completed app

--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.smuarem.deadpoolandwolverineheros.data.HeroesRepo
 import com.smuarem.deadpoolandwolverineheros.ui.theme.AppTheme
+import com.smuarem.deadpoolandwolverineheros.view.components.HeroCard
+import com.smuarem.deadpoolandwolverineheros.view.screens.HeroApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,29 +28,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    HeroApp(modifier = Modifier.padding(innerPadding))
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    Image(painter = painterResource(id = R.drawable.deadpool1), contentDescription = null)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
