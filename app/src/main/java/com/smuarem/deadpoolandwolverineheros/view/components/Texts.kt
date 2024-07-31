6280e73ce63e2abbb2bf50b94f3c72238d8abb85
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 10:49:22 +0400

    created app texts

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/view/components/Texts.kt
@@ -0,0 +1,68 @@
+package com.smuarem.deadpoolandwolverineheros.view.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.smuarem.deadpoolandwolverineheros.ui.constant.ConstTypography
+
+/**
+ * Top App Bar Text.
+ *
+ * @param modifier Modifier to be applied to the layout
+ */
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    Text(text = "Deadpool & Wolverine", style = ConstTypography.TOP_APP_BAR)
+}
+
+
+/**
+ * Hero Name Text.
+ *
+ * @param modifier Modifier to be applied to the layout
+ * @param heroName String resource ID for the hero's name
+ */
+@Composable
+fun HeroName(@StringRes heroName: Int, modifier: Modifier = Modifier) {
+    Text(text = stringResource(id = heroName), style = ConstTypography.HERO_NAME)
+}
+
+
+/**
+ * Hero's Actor Name Text.
+ *
+ * @param modifier Modifier to be applied to the layout
+ * @param heroActor String resource ID for the hero's actor name
+ */
+@Composable
+fun HeroActor(@StringRes heroActor: Int, modifier: Modifier = Modifier) {
+    Text(text = stringResource(id = heroActor), style = ConstTypography.HERO_ACTOR)
+}
+
+
+/**
+ * Hero Name Text.
+ *
+ * @param modifier Modifier to be applied to the layout
+ * @param heroDescription String resource ID for the hero's super power
+ */
+@Composable
+fun HeroDescription(@StringRes heroDescription: Int, modifier: Modifier = Modifier) {
+    Text(text = stringResource(id = heroDescription), style = ConstTypography.DESCRIPTION)
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HeroNamePreview() {
+    Column {
+        TopAppBar()
+        HeroName(heroName = com.smuarem.deadpoolandwolverineheros.R.string.deadpool_name)
+        HeroActor(heroActor = com.smuarem.deadpoolandwolverineheros.R.string.deadpool_actor)
+        HeroDescription(heroDescription = com.smuarem.deadpoolandwolverineheros.R.string.deadpool_superpower)
+    }
+}
