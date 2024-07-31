9aefd188696d6d70a94b08b6e3c2ee01942a3521
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 15:20:00 +0400

    completed app

--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/MainActivity.kt
@@ -4,21 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.smuarem.deadpoolandwolverineheros.data.HeroesRepo
 import com.smuarem.deadpoolandwolverineheros.ui.theme.AppTheme
-import com.smuarem.deadpoolandwolverineheros.view.components.HeroCard
 import com.smuarem.deadpoolandwolverineheros.view.screens.HeroApp
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/smuarem/deadpoolandwolverineheros/view/components/TopAppBar.kt
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/view/components/TopAppBar.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import com.smuarem.deadpoolandwolverineheros.ui.constant.ConstDimension
 
@@ -22,7 +22,7 @@ fun HeroTopAppBar(modifier: Modifier = Modifier) {
             top = dimensionResource(
                 id = ConstDimension.PADDING_LARGE
             )
-        ).fillMaxWidth()
+        ).fillMaxWidth().background(MaterialTheme.colorScheme.secondaryContainer)
     ) {
         TopAppBar()
     }
