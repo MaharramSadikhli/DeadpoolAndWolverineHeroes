7fe4168897fc8ed7935eaf84a7437250d00a5238
maharram sadikhli <s.muarem10@gmail.com>
2024-07-31 11:21:08 +0400

    created hero info col

---
+++ app/src/main/java/com/smuarem/deadpoolandwolverineheros/view/components/HeroInfo.kt
@@ -0,0 +1,37 @@
+package com.smuarem.deadpoolandwolverineheros.view.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.smuarem.deadpoolandwolverineheros.data.HeroesRepo
+import com.smuarem.deadpoolandwolverineheros.ui.constant.ConstDimension
+
+
+@Composable
+fun HeroInfo (
+    @StringRes heroName: Int,
+    @StringRes heroActor: Int,
+    modifier: Modifier = Modifier
+) {
+
+    val paddingSmall = dimensionResource(id = ConstDimension.PADDING_SMALL)
+    val paddingMedium = dimensionResource(id = ConstDimension.PADDING_MEDIUM)
+
+    Column(modifier = modifier.padding(paddingMedium)) {
+        HeroName(heroName = heroName, modifier.padding(bottom = paddingSmall))
+        HeroActor(heroActor = heroActor)
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HeroInfoPreview() {
+    val deadpool = HeroesRepo.heroes.first()
+    HeroInfo(heroName = deadpool.heroName, heroActor = deadpool.heroActor)
+}
